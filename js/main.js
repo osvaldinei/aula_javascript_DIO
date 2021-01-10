e58f891410cc55f619d96223262dd18b78e5c94a
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-01-10 18:19:31 -0300

    Aula1 - Introdução

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Osvaldinei Oliveira";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+
+console.log(n1*n2);
+
+console.log(frase.replace("Japão", "Brasil"));
+// alert(idade+idade2);
+// alert("Hello " + nome + "Você tem " + idade +" anos");
+//alert("Hello World JS")
