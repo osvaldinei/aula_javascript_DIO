77e21162f68f8e31b5e10f155f505fc5b8061603
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-01-10 18:26:01 -0300

    Aula2 - Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-var nome = "Osvaldinei Oliveira";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo";
+// var nome = "Osvaldinei Oliveira";
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Japão é o melhor time do mundo";
 
-console.log(n1*n2);
+// console.log(n1*n2);
 
-console.log(frase.replace("Japão", "Brasil"));
-// alert(idade+idade2);
-// alert("Hello " + nome + "Você tem " + idade +" anos");
-//alert("Hello World JS")
+// console.log(frase.replace("Japão", "Brasil"));
+// // alert(idade+idade2);
+// // alert("Hello " + nome + "Você tem " + idade +" anos");
+// //alert("Hello World JS")
+
+// var lista = ["maçã", "pêra", "uva"];
+// lista.push("laranja");
+// console.log(lista[1]);
+// console.log(lista);
+// console.log(lista.toString);
+var fruta = [{nome:"maçã", cor:"vermelha"}, {nome:"uva",cor:"roxa"}];
+alert(fruta[1].nome);
+// var fruta = {nome:"maçã", cor:"vermelha"};
+// alert(fruta.nome);
