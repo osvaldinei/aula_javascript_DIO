4043945bdb71389a93405fb689975ef1abddb9dc
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-01-11 21:07:36 -0300

    Aula4 - Desenvolva paginas web com JavaScript

--- js/main.js
+++ js/main.js
@@ -40,6 +40,27 @@
 //       alert(count);
 // }
 
-var d = new Date();
+// var d = new Date();
 
-alert(d.getMinutes());
+// alert(d.getMinutes());
+
+function soma(n1, n2){
+      return n1 + n2;
+}
+
+function validaIdade(idade){
+      var validar;
+      if(idade >= 18){
+           validar = true; 
+      }else{
+            validar = false;
+      }
+      return validar;
+}
+
+var idade = prompt('Qual a sua idade?');
+console.log(validaIdade(idade));
+
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
